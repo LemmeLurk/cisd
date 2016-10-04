01218138a51f3dfe5b9dcc828b85eeb86ded1d51
vinxi_Home <vinxi@live.com>
2016-10-03 21:56:02 -0700

    This is a half way through commit

--- scraper.py
+++ scraper.py
@@ -1,3 +1,6 @@
+# TESTING
+from urllib2 import urlopen
+# TESTING
 import shutil
 import os
 import re
@@ -20,17 +23,31 @@ class Workshop:
 
     def __init__ (self, section_title, chapter_number):
 
-        self.section_number = section_number
+        self.section_title = section_title
 
         self.chapter_number = chapter_number
 
-        self.directory = section_title + '/Chapter' + chapter_number
+        self.workshop_url = get_Workshop_Url (section_title, chapter_number)
 
-        self.files = None
-    
+        self.soup = get_Soup (self.workshop_url)
 
+        self.directory = section_title + '/Chapter' + str (chapter_number)
+
+        prepare_Directory (self.directory)
+
+        self.download_links = get_Download_Links (self.soup)
 
 # # 
+
+    
+def get_Download_Links (web_page):
+
+    string_pattern = re.compile (r'\bdownload')
+
+    links = web_page.find_all ('a',  attrs={'title' : string_pattern});
+
+    return links;
+
 
 def get_Soup (url):
 
@@ -39,19 +56,33 @@ def get_Soup (url):
     return BeautifulSoup (DOM, "html.parser");
 
 
+def get_File_Name (link):
+
+    return re.sub (r"(.*/.*/.*/)", "", link['href'])
+
+
+
 def get_File_Names (links):
 
     file_names = []
-
-    i = 0
 
     for link in links:
 
         file_names.append (re.sub (r"(.*/.*/.*/)", "", link['href']))
 
-        i += 1
-
     return file_names;
+
+
+def prepare_Directory (pathname):
+
+    if os.path.exists (pathname):
+
+# TODO Don't clobber
+        shutil.rmtree (pathname)
+
+    os.makedirs (pathname)
+
+    return; 
 
 
 def get_Directories (section, filenames):
@@ -79,6 +110,27 @@ def get_Directories (section, filenames):
     return directories;
 
 
+def get_Workshop_Url (class_section, chapter_number):
+
+    url = ""
+
+    if chapter_number <= 9:
+
+        o_number = '0' + str (chapter_number)
+
+        url = "http://zeus.mtsac.edu/~rpatters/CISD11/Workshops/"   \
+            + "Workshop_"   + o_number + "/Workshop" + o_number     \
+            + "/21694/"     + class_section 
+
+    else:
+
+        url =   "http://zeus.mtsac.edu/~rpatters/CISD11/Workshops/"     \
+            +   "Workshop_" + str (chapter_number)  + "/Workshop" +     \
+            +   str (chapter_number)   + "/21694/"  + class_section 
+
+    return url; 
+
+
 def get_Workshop_Urls (class_section):
 
     urls = [] 
@@ -103,38 +155,39 @@ def get_Workshop_Urls (class_section):
 
         urls.append (url)
 
-# TESTING
-    print ("\n"+"get_Workshop_Urls -- urls = \n")
-    print (urls)
-    print ("\n")
-# TESTING
     return urls; 
 
 
-def get_Download_Links (web_page):
-
-    string_pattern = re.compile (r'\bdownload')
-
-    links = web_page.find_all ('a',  attrs={'title' : string_pattern});
-
-# TESTING
-    print ("\nget_Download_Links() -- links = \n")
-    print (links)
-    print ("\n")
-# TESTING
-
-    return links;
     
 
 def download_File (link, directory, filename):
 
-    request = urllib2.Request (link)
+    # TESTING
+    print ('\n\ndownload_File() :: link: '+link+'\n\n')
+    # TESTING
+
+    #opener = urllib2.build_opener(proxy)
+    #opener.addheaders = {'User-agent':'Custom user agent'}
+    #urllib2.install_opener(opener)
+
+    request = urllib2.Request(link, headers={'User-Agent':'Mozilla/5.0 '\
+        +'(Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) '\
+        +'Chrome/44.0.2403.107 Safari/537.36','Upgrade-Insecure-Requests': \
+        '1','x-runtime': '148ms'})
+
+    request.headers['User-agent'] = 'Custom user agent'
+
+    request.add_header('User-agent', 'Custom user agent')
+    
+    request.add_unredirected_header ('User-Agent', 'Custom User-Agent') 
 
     response = urllib2.urlopen (request)
 
-    #fileName = re.search(r"(?<=Solve/)[^}]*(?=\.)", newLink).group(0)
+    os.chdir (directory)
 
-    file_stream = open (directory+"/"+filename, 'wb')
+    file_stream = open (filename, 'wb')
+
+    #download = wget.download (link)
 
     file_stream.write (response.read ())
 
@@ -143,28 +196,23 @@ def download_File (link, directory, filename):
     return;
 
 
-def CISD_Scraper (class_section):
+def Scrape (section):
 
-    workshop_urls = get_Workshop_Urls (class_section);
+    for i in range (1, 16):
 
-    for url in workshop_urls:
-
-        soup = get_Soup (url)
+        workshop = Workshop (section, i)
         
-        download_links = get_Download_Links (soup)
-        
-        filenames = get_File_Names (download_links)
-
-        directories = get_Directories (filenames)
-
-        for link, directory in zip (download_links, directories):
+        for a_tag in workshop.download_links:
 
             download_link = \
-                re.sub(r"(../../../)", __LINK_CONSTANT, link['href'])
+                re.sub(r"(../../../)", __LINK_CONSTANT, a_tag['href'])
 
-            download_File (download_link, directory)
+            filename = get_File_Name (a_tag)
+
+            download_File (download_link, workshop.directory, filename)
 
     return;
 
 
-CISD_Scraper (__CLASS_SECTIONS[0])
+Scrape (__CLASS_SECTIONS[0])
+Scrape (__CLASS_SECTIONS[1])
